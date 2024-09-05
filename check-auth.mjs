b9117d9d14a42a4c2efb59966cc3a6a55e4024bd
Jaime Futter <ST10067405@vcconnect.edu.za>
2024-09-05 14:30:47 +0200

    fixed grammar

--- check-auth.mjs
+++ check-auth.mjs
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 const checkauth=(req,res,next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "this_secret_shoyuld_be_than_it_is")
+        jwt.verify(token, "this_secret_should_be_longer_than_it_is")
         next();//passes control to the next handler
     } catch (error) {
         res.status(401).json({
